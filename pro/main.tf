4a6b25c00fb53ccd87554790ce02436f8ad19340
alv1981 <47571567+alv1981@users.noreply.github.com>
2019-07-05 08:59:59 +0200

    Update main.tf

--- pro/main.tf
+++ pro/main.tf
@@ -1,10 +1,6 @@
 provider "azurerm" {
 }
-resource "azurerm_resource_group" "rg" {
-        name = "${var.rg_name}"
-        location = "westus"
-        tags = {
-        environment = "aks-pro"
+
     }
 } 
 module "red" {
