4734b2873fbf4754e4e99d884d4c96e4dc1bb040
alv1981 <47571567+alv1981@users.noreply.github.com>
2019-07-08 09:46:32 +0200

    Update main.tf

--- pro/main.tf
+++ pro/main.tf
@@ -1,15 +1,16 @@
 provider "azurerm" {
 }
-#data "terraform_remote_state" "pro" {
- # backend = "azurerm"
-  #  storage_account_name = "tfstalabscastillo"
-   # container_name       = "tfstate"
-    #key                  = "prod.terraform.tfstate"
+data "terraform_remote_state" "pro" {
+  backend = "azurerm"
+  config = {
+    storage_account_name = "tfstalabscastillo"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
 
    
     
-  #}
-#}
+  }
+}
     
  
 module "red" {
