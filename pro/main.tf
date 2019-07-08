559ace3753e1a1ca55d1789790f22a9a24a92de3
alv1981 <47571567+alv1981@users.noreply.github.com>
2019-07-08 10:07:02 +0200

    Update main.tf

--- pro/main.tf
+++ pro/main.tf
@@ -5,8 +5,9 @@ data "terraform_remote_state" "pro" {
   config = {
     storage_account_name = "tfstalabscastillo"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "prod/aks.tfstate"
     resource_group_name  = "aks-pro" 
+    access_key           = "5hdJ0UfY6zX2hwddnllVhBRLOmpgystxIyMmhoBcira+MSdRPLLMeHTExn4JFIscPDuL/4TkhKbl/6VVX9lIdA=="
    
     
   }
