e899af4cd3151da907bddc9beb59521f59ef7394
root <acasve1981@gmail.com>
2019-07-09 08:47:28 +0200

    eliminiado credenciales

--- variables_access_azure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Setting environment variables for Terraform"
-export ARM_SUBSCRIPTION_ID="4b97c60d-fbe7-44ed-853e-488bd6750119"
-export ARM_CLIENT_ID="77748164-d291-4b73-b885-ad10ed378bda"
-export ARM_CLIENT_SECRET="09fdefd0-0c12-4db5-b0fe-743b75a0d5c7"
-export ARM_TENANT_ID="64d23cb6-425e-42de-9052-c2aa0063ff9d"
